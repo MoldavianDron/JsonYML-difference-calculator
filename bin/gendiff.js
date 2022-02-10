c3df2dc916d19d7cfa904636252e6af37e5467ca
Andrei Zaporojan <moldaviandron@gmail.com>
2022-02-10 23:11:39 +0200

    Task 2: Add args to gendiff cli utility

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,14 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference')
-  .version('1.0.0');
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference');
+
+program
+  .option('-f, --format <type>', 'output format');
+
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
