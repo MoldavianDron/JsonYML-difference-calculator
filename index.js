c6cc4a66a4a1e0d15149d384b171bf09ce342080
Andrei Zaporojan <moldaviandron@gmail.com>
2022-02-16 22:27:59 +0200

    Task 3 refactor

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import _ from 'lodash';
 import path from 'path';
 import process from 'process';
 import fs from 'fs';
+import diffStringify from './src/diffStringify.js';
 
 const areValid = (path1, path2) => {
   const validFormats = ['json', 'yml'];
@@ -18,48 +19,12 @@ const areValid = (path1, path2) => {
   const fileName2 = _.last(path2.split('/'));
   const format1 = _.last(fileName1.split('.'));
   const format2 = _.last(fileName2.split('.'));
-  return (format1 === format2 && validFormats.includes(format1));
-};
-
-const diffStringify = (obj1, obj2, flagedObject, sep = ' ', repeats = 1) => {
-  const iter = (curValue, depth) => {
-    if (!_.isObject(curValue)) {
-      return `${curValue}`;
-    }
-    const flagDiff = (key, flag, curIndent) => {
-      switch (flag) {
-      case 'deleted':
-        return `${curIndent} - ${key}: ${obj1[key]}`;
-      case 'unchanged':
-        return `${curIndent}   ${key}: ${obj1[key]}`;
-      case 'added':
-        return `${curIndent} + ${key}: ${obj2[key]}`;
-      default:
-        return [
-          `${curIndent} - ${key}: ${obj1[key]}`,
-          `${curIndent} + ${key}: ${obj2[key]}`
-        ];
-      }
-    }
-    const indentSize = repeats * depth;
-    const indent = sep.repeat(indentSize);
-    const closeBracketIndent = sep.repeat(indentSize - repeats);
-    const lines = Object
-      .entries(curValue)
-      .flatMap(([key, flag]) => flagDiff(key, flag, indent));
-    return [
-      '{',
-      ...lines,
-      `${closeBracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(flagedObject, 1);
+  return (validFormats.includes(format1) && validFormats.includes(format2));
 };
 
 const genDiff = (pathToFile1, pathToFile2) => {
   if (!areValid(pathToFile1, pathToFile2)) {
-    return new Error('Both files must have .json or .yml extensions')
+    return new Error('File extension must be .json or .yml')
   }
 
   const fullPath1 = path.resolve(process.cwd(), pathToFile1);
@@ -68,26 +33,8 @@ const genDiff = (pathToFile1, pathToFile2) => {
   const obj1 = JSON.parse(fs.readFileSync(fullPath1));
   const obj2 = JSON.parse(fs.readFileSync(fullPath2));
   const mergedObj = { ...obj1, ...obj2 };
-
-  const allSortedKeys = Object.keys(mergedObj).sort();
-  const flagedObject = allSortedKeys.reduce(
-    (acc, key) => {
-      if (_.has(obj1, key) && _.has(obj2, key)) {
-        acc[key] = obj1[key] === obj2[key] ? 'unchanged' : 'changed';
-      }
-      if (!_.has(obj1, key)) {
-        acc[key] = 'added';
-      }
-
-      if (!_.has(obj2, key)) {
-        acc[key] = 'deleted'
-      }
-      return acc;
-    },
-    {}
-  )
   
-  return diffStringify(obj1, obj2, flagedObject, ' ', 1);
+  return diffStringify(obj1, obj2, mergedObj);
 };
 
 export default genDiff;
